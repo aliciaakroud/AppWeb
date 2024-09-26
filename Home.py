743cbb410ae2fd56029255770c82746be5d8f6c2
aliciaakroud <alicia.akroud@eemi.com>
2024-09-26 16:51:44 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -11,14 +11,6 @@ user_input = st.text_input("Tapez votre nom: ")
 
 user_input = st.text_input("Tapez votre prénom: ")
 
-#image
-st.video("https://youtu.be/kK3gGPkO9L8")
-
-
-#Sidebare
-st.sidebar.image("https://cdn.dribbble.com/users/2018170/screenshots/14771091/media/8244197cd8d6a675f0fca573bfb5498f.png?compress=1&resize=400x300")
-st.sidebar.title("Alicia Akroud")
-
 #Select bare
 st.selectbox("Selectionnez votre niveau d'étude", ["Bac", "Bac +2", "Bac +5"])
 
@@ -28,3 +20,16 @@ if age < 18 :
     st.write("Vous etes mineur")
 else: 
     st.write("Vous etes majeur")
+    
+#image
+#st.video("https://youtu.be/kK3gGPkO9L8")
+
+
+#Sidebare
+st.sidebar.title("Alicia Akroud")
+st.sidebar.image("https://cdn.dribbble.com/users/2018170/screenshots/14771091/media/8244197cd8d6a675f0fca573bfb5498f.png?compress=1&resize=400x300")
+
+
+
+
+
