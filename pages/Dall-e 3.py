0444e9c476209efdc8080747d6e0541090585dc0
aliciaakroud <alicia.akroud@eemi.com>
2024-09-26 16:17:39 +0200

    Update Dall-e 3.py

--- pages/Dall-e 3.py
+++ pages/Dall-e 3.py
@@ -1,33 +1,24 @@
 import streamlit as st
-import openai  # Importer le module OpenAI correctement
+from openai import OpenAI
 
-st.title("Dalle-e 3")
-st.write("Veuillez entrer une description de l'image que vous souhaitez générer")
-description = st.text_input("Veuillez saisir votre description :")
-st.write(description)
-st.sidebar.title("Mariama Diallo")
+#Titre
+st.title("Dall-e 3")
 
-cle = st.sidebar.text_input("Veuillez saisir votre clé OpenAI", type="password")
+#Champ de saisi
+user_input = st.text_input("Veuillez entrer une description de l'image que vous souhaitez générer : ")
+st.write(user_input)
 
-# Vérifiez que la clé est bien fournie
-if cle:
-    openai.api_key = cle  # Assigner la clé API correctement
+#Sidebare
+open_ia_key = st.sidebar.text_input("Veuillez entrer la clé Open IA")
+st.write(open_ia_key)
 
-    # Vérifiez que l'utilisateur a entré une description
-    if description:
-        # Appeler l'API pour générer une image avec DALL-E
-        response = openai.Image.create(
-            prompt=description,
-            n=1,
-            size="512x512"
-        )
-
-        # Récupérer l'URL de l'image générée
-        image_url = response['data'][0]['url']
-        st.image(image_url, caption=f"Image générée pour : {description}")
-
-    else:
-        st.write("Veuillez entrer une description pour générer une image.")
-else:
-    st.write("Veuillez entrer votre clé API dans la barre latérale.")
+client = OpenAI(api_key=open_ia_key)
+prompt=user_input,
+st.image = client.image.create(
+    model="dall-e-2",
+    prompt=user_input,
+    size="512x512",
+    quality="standard",
+    n=1,
+)
  
