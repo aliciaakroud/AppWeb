572c4506d0c11d177191b9e59f0e61ef8624f672
aliciaakroud <alicia.akroud@eemi.com>
2024-09-26 15:38:38 +0200

    Update Dall-e 3.py

--- pages/Dall-e 3.py
+++ pages/Dall-e 3.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from openai import OpenAI
 
 #Titre
 st.title("Dall-e 3")
@@ -7,11 +8,16 @@ st.title("Dall-e 3")
 user_input = st.text_input("Veuillez entrer une description de l'image que vous souhaitez générer : ")
 st.write(user_input)
 
-#image
-#st.image(" ")
-
 #Sidebare
 open_ia_key = st.sidebar.text_input("Veuillez entrer la clé Open IA")
 st.write(open_ia_key)
 
+open_ia_key = OpenAI(api_key=OpenAIKEY)
 
+st.image = open_ia_key.images.generate(
+    model="dall-e-2",
+    prompt=prompt,
+    size="512x512",
+    quality="standard",
+    n=1,
+)
