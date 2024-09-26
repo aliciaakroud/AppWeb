30a44287776e28de57963f417a02467329836c04
aliciaakroud <alicia.akroud@eemi.com>
2024-09-26 16:42:41 +0200

    Update Dall-e 3.py

--- pages/Dall-e 3.py
+++ pages/Dall-e 3.py
@@ -5,11 +5,10 @@ st.title("Dall-e 3")
 
 #Champ de saisi
 user_input = st.text_input("Veuillez entrer une description de l'image que vous souhaitez générer : ")
-st.write(user_input)
+
 
 #Sidebare
 open_ia_key = st.sidebar.text_input("Veuillez entrer la clé Open IA")
-st.write(open_ia_key)
 
 client = OpenAI(api_key=open_ia_key)
 
