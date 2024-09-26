5d55323ff0f8fc6e9effd8a2f6767ac6aa496392
aliciaakroud <alicia.akroud@eemi.com>
2024-09-26 15:21:47 +0200

    Update Dall-e 3.py

--- pages/Dall-e 3.py
+++ pages/Dall-e 3.py
@@ -1,3 +1,16 @@
 import streamlit as st
 
-st.title("Dall-e 3")
+#Titre
+st.title("Application Web - Open IA")
+
+#Champ de saisi
+user_input = st.text_input("Veuillez entrer une description de l'image que vous souhaitez générer : ")
+st.write(user_input)
+
+#image
+st.image(" ")
+
+#Sidebare
+st.sidebar.text_input("Veuillez entrer la clé Open IA")
+st.write(user_input)
+
